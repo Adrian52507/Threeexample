0ed9673fc06f0502933316f228c6e4fe1997262e
Adrian <77292841+Adrian52507@users.noreply.github.com>
2025-05-19 19:33:17 -0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -14,7 +14,7 @@ document.body.appendChild(renderer.domElement);
 
 // Crear un cubo
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: "#982649" });
+const material = new THREE.MeshBasicMaterial({ color: "green" });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
